deb04feedefb0017b39f4f6d144ebd709bdc78ca
Xitij <kshitijvsx@gmail.com>
2025-08-24 15:37:39 +0530

    VimCrypt~03!ãÑlȃ~ÈI]ydDwÓà]	¸V¾¼3M¸¶_çßsLJC!Û qu6ÕR3ãq(; ѰHÞjç¯s=âldqD,ۺڰD͂(úq<Xö²Vп9À»PlÀæ¿=·VwÖs÷÷X¯Ǭ5ËÞ}]ںZ'd	X £xٮ#CVñ	PÉÙL´	0hßÆvåÏlùuÃîmÇzS)Rr¡S/Rk2)+Äpë0ϤÝãø®DÊk¨§¼umrïhº hs¨0+mI«iäoüu"i/æ4èÍ

--- LLMs/script.bat
+++ LLMs/script.bat
@@ -10,12 +10,18 @@ docker build -t ollama .
 docker-compose exec ollama ollama pull nomic-embed-text:latest
 docker-compose exec ollama ollama pull llama:latest
 docker-compose exec ollama ollama pull smollm2:latest
+docker-compose exec ollama ollama pull codegemma:latest
 
 # List the downloaded models inside Ollama container
 docker-compose exec ollama ollama ls
 
 # Run the Python app container interactively
-docker-compose run --rm -it app
+# docker-compose run --rm -it app
 
-docker build -t app
+# docker build -t app
+
+# docker-compose run --rm -it app sh
+
+docker-compose run --rm app python main.py
+
 
